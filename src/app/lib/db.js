67b76ac698e34a538fd14c37e5f3df57cebf83d0
Ruturaj02 <121890966+Ruturaj02@users.noreply.github.com>
2025-10-05 02:14:16 +0530

    coonect to  mongodb and create some sample schema

---
+++ src/app/lib/db.js
@@ -0,0 +1,3 @@
+// const { username, password } = process.env;
+
+export const connectionStr ="mongodb+srv://ruturajs234_db_user:Rutu123@cluster0.snfkofv.mongodb.net/EATs?retryWrites=true&w=majority&appName=Cluster0"
