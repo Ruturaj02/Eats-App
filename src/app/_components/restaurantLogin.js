7d0ba8af6c35f03bdddf0e4641b716679c3b2e4c
Ruturaj02 <121890966+Ruturaj02@users.noreply.github.com>
2025-10-23 12:14:03 +0530

    from validation and handling in login page..

--- src/app/_components/restaurantLogin.js
+++ src/app/_components/restaurantLogin.js
@@ -1,4 +1,21 @@
+"use client";
+
+import { useState } from "react";
+
 const RestaurantLogin = () => {
+  const [email,setEmail] = useState();
+  const [password,setPassword] = useState();
+  const [error,setError] = useState(false);
+  
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if(!email || !password){
+      setError(true);
+    }else{
+      setError(false);
+    }
+    console.log(email,password);
+  }
   return (
     <>
       <h3>Login</h3>
@@ -9,17 +26,23 @@ const RestaurantLogin = () => {
               type="text"
               placeholder="Enter Email id "
               className="input-field"
+              value={email}
+              onChange={(e)=>setEmail(e.target.value)}
             />
+            {error && !email && <span className="input-error">Email is required</span>}
           </div>
           <div className="input-wrapper">
             <input
               type="password"
               placeholder="Enter password"
               className="input-field"
+              value={password}
+              onChange={(e)=>setPassword(e.target.value)}
             />
+            {error && !password && <span className="input-error">Password is required</span>}
           </div>
           <div className="input-wrapper">
-            <button className="button" type="submit">
+            <button className="button" type="submit" onClick={handleLogin}>
               Login
             </button>
           </div>
