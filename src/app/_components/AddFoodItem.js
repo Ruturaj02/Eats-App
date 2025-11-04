2b65eefb6b6ebe31fec6534527b93bd96e38cc52
Ruturaj02 <ruturajs234@gmail.com>
2025-11-04 13:24:59 +0530

    make add food schema,api integration and form validation

--- src/app/_components/AddFoodItem.js
+++ src/app/_components/AddFoodItem.js
@@ -1,37 +1,102 @@
-import { useState } from "react"
-
+import { useState } from "react";
 
 const AddFoodItem = () => {
-    const [name,setName] = useState('');
-    const [price,setPrice] = useState('');
-    const [path,setPath] = useState('');
-    const [description,setDescription] = useState('');
-    const handleAddFood = ()=>{
-        console.log({name,price,path,description});
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [path, setPath] = useState("");
+  const [description, setDescription] = useState("");
+  const [error,setError] = useState(false);
+
+  const handleAddFood = async () => {
+    console.log({ name, price, path, description });
+    if(!name || !price || !path || !description){
+        setError(true);
+        return false;
+    }else{
+        setError(false);
     }
+    let resto_id;
+    const restaurantData = JSON.parse(localStorage.getItem("restaurantUser"));
+    if (restaurantData) {
+      resto_id = restaurantData._id;
+    }
+    let response = await fetch("http://localhost:3000/api/restaurant/foods", {
+      method: "POST",
+      body: JSON.stringify({
+        name,
+        price,
+        img_path: path,
+        description,
+        resto_id,
+      }),
+    });
+    response = await response.json();
+    if (response) {
+      alert("Food item Added");
+    }else{
+        alert("Not added food item");
+    }
+  };
   return (
-  <>
-    <div className="container">
+    <>
+      <div className="container">
         <h1>Add Food Item</h1>
         <div className="input-wrapper">
-        <input type="text" placeholder="Enter food name" value={name} onChange={(e)=>setName(e.target.value)} className="input-field" />
-    </div>
-     <div className="input-wrapper">
-        <input type="text" placeholder="Enter Price" value={price} onChange={(e)=>setPrice(e.target.value)} className="input-field" />
-    </div>
-     <div className="input-wrapper">
-        <input type="text" placeholder="Enter image path" value={path} onChange={(e)=>setPath(e.target.value)} className="input-field" />
-    </div>
-     <div className="input-wrapper">
-        <input type="text" placeholder="Enter description" value={description} onChange={(e)=>setDescription(e.target.value)} className="input-field" />
-    </div>
-    <div className="input-wrapper">
-        <button onClick={handleAddFood} className="button">Add Food</button>
-    </div>
-    </div>
-    
-  </>
-  )
-}
+          <input
+            type="text"
+            placeholder="Enter food name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="input-field"
+          />
+          {
+            error && !name && <span className="input-error">* Enter valid name</span>
+          }
+        </div>
+        <div className="input-wrapper">
+          <input
+            type="text"
+            placeholder="Enter Price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            className="input-field"
+          />
+          {
+            error && !price && <span className="input-error">* Enter valid price</span>
+          }
+        </div>
+        <div className="input-wrapper">
+          <input
+            type="text"
+            placeholder="Enter image path"
+            value={path}
+            onChange={(e) => setPath(e.target.value)}
+            className="input-field"
+          />
+          {
+            error && !path && <span className="input-error">* Enter valid image path</span>
+          }
+        </div>
+        <div className="input-wrapper">
+          <input
+            type="text"
+            placeholder="Enter description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            className="input-field"
+          />
+          {
+            error && !description && <span className="input-error">* Enter valid description</span>
+          }
+        </div>
+        <div className="input-wrapper">
+          <button onClick={handleAddFood} className="button">
+            Add Food
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default AddFoodItem
+export default AddFoodItem;
--- src/app/lib/foodsModel.js
+++ src/app/lib/foodsModel.js
@@ -1,12 +1,13 @@
-import mongoose from "mongoose";
+const { default: mongoose } = require("mongoose");
 
 const foodsModel = new mongoose.Schema({
     name:String,
-    price:String,
+    price:Number,
     img_path:String,
     description:String,
     resto_id:mongoose.Schema.Types.ObjectId
 
-});
+})
 
-export const foodSchema = mongoose.models.foods || mongoose.model("foods",foodsModel);
+
+export const foodsSchema = mongoose.models.foods || mongoose.model("foods",foodsModel);
