9cc8bb889457dc1491ecddddd71d7c197f54b010
Ruturaj02 <ruturajs234@gmail.com>
2025-11-12 16:52:59 +0530

    added food itemlist component and api

---
+++ src/app/_components/FoodItemList.js
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react"
+
+const FoodItemList = () => {
+    const [foodItems, setFoodItems] = useState()
+
+    useEffect(() => {
+        loadFoodItems();
+    }, [])
+
+
+    const loadFoodItems = async () => {
+        const restaurantData = JSON.parse(localStorage.getItem("restaurantUser"));
+        const resto_id = restaurantData._id;
+        let response = await fetch(`http://localhost:3000/api/restaurant/foods/${resto_id}`);
+        response = await response.json();
+        // console.log(response);
+        if (response.success) {
+            setFoodItems(response.result);
+        } else {
+            alert("Error while loading food items");
+        }
+    }
+    return (
+        <div>
+            <h1>Food Items</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <td>S.N</td>
+                        <td>Name</td>
+                        <td>Price</td>
+                        <td>Description</td>
+                        <td>Image</td>
+                        <td>Operations</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        foodItems && foodItems.map((item, i) => (
+                            <tr key={i}>
+                                <td>{i}</td>
+                                <td>{item.name}</td>
+                                <td>{item.price}</td>
+                                <td>{item.description}</td>
+                                <td><img src={item.img_path} alt="loading" /></td>
+                                <td><button>Delete</button><button>Edit</button></td>
+                            </tr>
+                        ))
+                    }
+
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default FoodItemList
--- src/app/restaurant/dashboard/page.js
+++ src/app/restaurant/dashboard/page.js
@@ -4,6 +4,7 @@ import RestaurantHeader from "@/app/_components/RestaurantHeader";
 import "../style.css";
 import AddFoodItem from "@/app/_components/AddFoodItem";
 import { useState } from "react";
+import FoodItemList from "@/app/_components/FoodItemList";
 
 const page = () => {
   const [addItem, setAddItem] = useState(false);
@@ -13,7 +14,7 @@ const page = () => {
         <RestaurantHeader />
         <button onClick={()=>setAddItem(true)}>Add Food</button>
         <button onClick={()=>setAddItem(false)}>Dashboard</button>
-        {addItem ? <AddFoodItem /> : <h1>Welcome to Dashboard Page</h1>}
+        {addItem ? <AddFoodItem /> : <FoodItemList/>}
       </div>
     </>
   );
