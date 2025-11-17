8b6a3a7941215ed423472b6bb693e02faa5015d1
Ruturaj02 <ruturajs234@gmail.com>
2025-11-17 18:21:41 +0530

    add locations search api and integrate it on ui , show and hide placed on search place tab

---
+++ src/app/api/customer/locations/route.js
@@ -0,0 +1,12 @@
+import { connectionStr } from "@/app/lib/db";
+import { restaurantSchema } from "@/app/lib/restaurantModel";
+import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+
+export async function GET(request,content){
+    await mongoose.connect(connectionStr);
+    let result = await restaurantSchema.find();
+    result = result.map((item)=>item.city.charAt(0).toUpperCase()+item.city.slice(1));
+    result = [...new Set(result)];
+    return NextResponse.json({result,success:true});
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,61 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { set } from "mongoose";
+// import styles from "./page.module.css";
 import CustomerHeader from "./_components/CustomerHeader";
 import Footer from "./_components/Footer";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [locations, setLocations] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState("");
+  const [showLocations, setShowLocations] = useState(false);
+
+  useEffect(() => {
+    loadLocations();
+  }, []);
+
+  const loadLocations = async () => {
+    let response = await fetch(`http://localhost:3000/api/customer/locations`);
+    response = await response.json();
+    if (response.success) {
+      setLocations(response.result);
+    }
+  };
+  // console.log(locations);
+const handleListItem=(item)=>{
+  setSelectedLocation(item);
+  setShowLocations(false);
+}
+
+
   return (
     <div>
       <CustomerHeader />
       <div className="main-page-banner">
         <h1>EATS App</h1>
         <div className="input-wrapper">
-          <input type="text" className="select-input" placeholder="Select place" />
-          <input type="text" className="search-input" placeholder="Enter food or restaurant name" />
+          <input
+            type="text"
+            onClick={()=>setShowLocations(true)}
+            value={selectedLocation}
+            className="select-input"
+            placeholder="Select place"
+          />
+          <ul className="location-list">
+            {showLocations && locations.map((item,i) => (
+              <li key={i} onClick={()=>handleListItem(item)}>
+                {item}
+              </li>
+            ))}
+          </ul>
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Enter food or restaurant name"
+          />
         </div>
-
       </div>
       <Footer />
-
     </div>
   );
 }
